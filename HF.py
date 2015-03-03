bd607e78e2a0507463d0f9ad6fbea90be656934b
Sharjeel Aziz <saziz@caltech.edu>
2015-03-02 23:32:26 -0800

    Found the HD of all languages with respect to themselves

--- HF.py
+++ HF.py
@@ -14,26 +14,33 @@ with open('named_data.json', 'rb') as fp:
 			if p in data[lang]:
 				if 'Yes' in data[lang][p]:
 					res = res + "1"
+				else:
+					res = res + "0"
 			else:
 				res = res + "0"
-		print res
-		num_data[lang] = int(res, 2)
+		# print res
+		num_data[lang] = res
 
-def hamming_distance(a, b):
-	dist = 0
-	val = a ^ b
-	print b
-	while val is not 0:
-		dist = dist +1
-		val = val & (val-1)
-	return dist
+# def hamming_distance(a, b):
+# 	dist = 0
+# 	val = a ^ b
+# 	# print b
+# 	while val is not 0:
+# 		dist = dist + 1
+# 		val = val & (val - 1)
+# 	return dist
+
+def hamming_distance(s1, s2):
+    """Return the Hamming distance between equal-length sequences"""
+    if len(s1) != len(s2):
+        raise ValueError("Undefined for sequences of unequal length")
+    return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
 
 d = {}
 for a in num_data:
 	d2 = {}
 	for b in num_data:
 		if a is not b:
-			print a
 			d2[b] = hamming_distance(num_data[a], num_data[b])
 	d[a] = d2
 
